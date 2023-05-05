7499d133e9131c729299e566f1e7b0cd7df53abe
Koder <frack52@hotmail.com>
2023-05-06 08:16:24 +0900

    Create notebook_env.py

---
+++ data/fraudfinder/config/notebook_env.py
@@ -0,0 +1,10 @@
+
+BUCKET_NAME          = "qwiklabs-gcp-02-e42b56359111-fraudfinder"
+PROJECT              = "qwiklabs-gcp-02-e42b56359111"
+REGION               = "us-central1"
+ID                   = "9dia6"
+FEATURESTORE_ID      = "fraudfinder_9dia6"
+MODEL_NAME           = "ff_model"
+ENDPOINT_NAME        = "ff_model_endpoint"
+TRAINING_DS_SIZE     = "1000"
+
